8648603e27695dbd789fc665c8ec736059009477
abdullah47044704-dotcom <abdullah47044704@gmail.com>
2026-07-12 22:28:24 +0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,49 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+URL = "https://apis.evotopup.com/api/game-id-checker"
 
-@app.route('/about')
-def about():
-    return 'About'
+HEADERS = {
+    "accept": "application/json",
+    "content-type": "application/json",
+    "Origin": "https://evotopup.com",
+    "Referer": "https://evotopup.com/",
+    "User-Agent": "Mozilla/5.0"
+}
+
+
+@app.route("/", methods=["GET"])
+def check_uid():
+    uid = request.args.get("uid")
+
+    if not uid:
+        return jsonify({
+            "status": False,
+            "message": "Missing uid parameter"
+        }), 400
+
+    payload = {
+        "playerid": uid
+    }
+
+    try:
+        response = requests.post(
+            URL,
+            json=payload,
+            headers=HEADERS,
+            timeout=15
+        )
+
+        return jsonify(response.json()), response.status_code
+
+    except Exception as e:
+        return jsonify({
+            "status": False,
+            "error": str(e)
+        }), 500
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
